9bcd2995b91a493f220ad6fe871fea2e3c82c0c7
jessegoodier <jessegoodier@gmail.com>
2020-04-22 13:17:56 -0400

    testing fixes

--- 0-install-required-dependencies.sh
+++ 0-install-required-dependencies.sh
@@ -5,7 +5,7 @@ apt install software-properties-common
 apt-add-repository --yes --update ppa:ansible/ansible
 apt -y install ansible
 ansible-galaxy install nginxinc.nginx 
-
-ansible-galaxy install nginxinc.nginx-controller-generate-token
+ansible-galaxy install nginxinc.nginx_controller_agent
+ansible-galaxy install nginxinc.nginx_controller_generate_token
 chmod 755 *.sh
 ssh-keyscan -H localhost > ~/.ssh/known_hosts
---
+++ hostroot.sh
@@ -0,0 +1,7 @@
+[loadbalancers]
+localhost ansible_user=root ansible_password=root
+
+
+[loadbalancers:vars]
+ansible_python_interpreter=/usr/bin/python2.7
+
