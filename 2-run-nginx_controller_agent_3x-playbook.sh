3f08cd5a5c9f58d0293e4662471b7be6aa6aff50
jessegoodier <jessegoodier@gmail.com>
2020-04-28 21:27:56 -0400

    update for new ansible playbooks

--- 2-run-nginx_controller_agent_3x-playbook.sh
+++ 2-run-nginx_controller_agent_3x-playbook.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ansible-playbook nginx_controller_agent_3x.yaml -b -i hosts -e "user_email=admin@nginx.com user_password=Nginx1122! controller_fqdn=controller1.ddns.net"
+ansible-playbook nginx_controller_agent_3x.yaml -b -i hosts
