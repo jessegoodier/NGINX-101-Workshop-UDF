3e32172a1b17d4c959716fd19c58c741ed1a0ae3
jessegoodier <jessegoodier@gmail.com>
2020-05-04 16:42:11 -0400

    fixed installer

--- 0-install-required-dependencies.sh
+++ 0-install-required-dependencies.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 git pull
-apt-add-repository --yes --update ppa:ansible/ansible
-apt-get update
-apt-get -y install python2.7 jq software-properties-common ansible
+sudo apt-get update
+sudo apt-get install software-properties-common
+sudo apt-add-repository --yes --update ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get -y install python2.7 jq software-properties-common ansible
 
 ansible-galaxy install nginxinc.nginx --force
 ansible-galaxy install nginxinc.nginx_controller_agent --force
