5fb7979354b300684dce1f9f615e5b8ce897608f
31039225+jessegoodier@users.noreply.github.com <31039225+jessegoodier@users.noreply.github.com>
2020-09-01 17:12:39 -0400

    update for controller 3.8

--- 0-install-required-dependencies.sh
+++ 0-install-required-dependencies.sh
@@ -6,9 +6,7 @@ sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get -y install python2.7 jq software-properties-common ansible
 
-ansible-galaxy install nginxinc.nginx --force
-ansible-galaxy install nginxinc.nginx_controller_agent --force
-ansible-galaxy install nginxinc.nginx_controller_generate_token --force
+ansible-galaxy collection install nginxinc.nginx_controller --force
 chown -R ubuntu:ubuntu /home/ubuntu/.ansible
 chmod 755 *.sh
 ssh-keyscan -H localhost > ~/.ssh/known_hosts
